b20fd15d6365d99ae88472a21ae9b9eeba31fe98
addpadding <tiktokaddison20@gmail.com>
2023-03-17 15:10:47 +0300

    54.1%

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,14 @@
 import Head from 'next/head'
+import { BsFillMoonStarsFill } from "react-icons/bs"
+
+import {
+    AiFillTwitterCircle,
+    AiFillLinkedin,
+    AiFillYoutube,
+} from "react-icons/ai";
+
+import Image from 'next/image';
+import deved from "../public/dev-ed-wave.png"
 
 export default function Home() {
     return (
@@ -10,11 +20,60 @@ export default function Home() {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
 
-            <main>
-                <section className=" bg-blue-700 min-h-screen">
+            <main className=' bg-white px-10'>
+
+                <section className=" min-h-screen">
+
+                    <nav className='py-10 mb-12 flex justify-between'>
+
+                        <h1 className=' text-xl font-burtons'>developedbyed</h1>
+
+                        <ul className=' flex items-center'>
+                            <li>
+                                <BsFillMoonStarsFill className=' cursor-pointer text-2xl' />
+                            </li>
+                            <li>
+                                <a className=' bg-gradient-to-t from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8' href="#">Resume</a>
+                            </li>
+                        </ul>
+
+                    </nav>
+
+                    <div className=' text-center p-10'>
+
+                        <h2 className=' text-5xl py-2 text-teal-600 font-medium'>
+                            Dimitri Marco
+                        </h2>
+
+                        <h3 className=' text-2xl py-2'>
+                            Developer and designer
+                        </h3>
+
+                        <p className=' text-md py-5 leading-5 text-gray-800'>
+                            Freelancer providing services for programming and design content
+                            needs. Join me down below and let's get cracking!
+                        </p>
+
+                    </div>
+
+                    <div className=' text-5xl flex justify-center gap-16 py-3 text-gray-600'>
+                        <AiFillTwitterCircle />
+                        <AiFillLinkedin />
+                        <AiFillYoutube />
+                    </div>
+
+                    <div className=' relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt-20 overflow-hidden'>
+                        <Image src={deved} alt="" layout='fill ' objectFit='cover' />
+                    </div>
 
                 </section>
+
+                <section>
+
+                </section>
+
             </main>
+
         </div>
     )
 }
